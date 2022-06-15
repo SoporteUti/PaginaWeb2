78e110752b087bae7ea3faf2e523b25c6089e230
lisgu <87437110+SoporteUti@users.noreply.github.com>
2022-06-15 13:29:42 -0600

    PROBANDO PROCEDIMIENTO ACTIVO

--- Sitio_Web_FMP/app/Http/Controllers/Licencias/ReporteController.php
+++ Sitio_Web_FMP/app/Http/Controllers/Licencias/ReporteController.php
@@ -137,7 +137,7 @@ class ReporteController extends Controller
         inner join jornada_items ji ON ji.id_jornada = jornada.id
         inner join reloj_datos r on e.dui=r.id_persona
         where e.id_depto=" . $request->id_depto . " and e.dui=r.id_persona
-        and jornada.estado='activo'
+        and jornada.procedimiento='aceptado' and periodos.estado='activo'
         and ji.dia=r.dia_semana and ji.hora_inicio::time+'00:05:59' < r.entrada::time
         and  to_char(r.fecha::date,'YYYY')::int=" . $request->anio . "
         and to_char(r.fecha::date,'MM')::int=" . $request->mes . " and r.entrada !='-'
@@ -538,7 +538,7 @@ class ReporteController extends Controller
             inner join jornada_items ji ON ji.id_jornada = jornada.id
             inner join reloj_datos r on e.dui=r.id_persona
             where e.id_depto=" . $request->id_depto . " and e.dui=r.id_persona
-            and jornada.estado='activo'
+            and jornada.procedimiento='aceptado' and periodos.estado='activo'
             and ji.dia=r.dia_semana 
             and  to_char(r.fecha::date,'YYYY')::int=" . $request->anio . "
             and to_char(r.fecha::date,'MM')::int=" . $request->mes . "
@@ -609,7 +609,7 @@ class ReporteController extends Controller
         inner join reloj_datos r on e.dui=r.id_persona
         where e.id_depto=" . $request->id_depto . " and e.dui=r.id_persona
         and ji.dia=r.dia_semana and r.entrada='-' and r.salida='-'
-        and jornada.estado='activo'
+        and jornada.procedimiento='aceptado' and periodos.estado='activo'
         and permisos.tipo_permiso='LS/GS' and permisos.estado='Aceptado'
         and  to_char(r.fecha::date,'YYYY')::int=" . $request->anio . "
         and to_char(r.fecha::date,'MM')::int=" . $request->mes . "
